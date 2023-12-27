e3ead7db423fa712342e3b30a6e0bd0ffc996e4e
Aryan Kumar <lci2020004@iiitl.ac.in>
2023-12-27 18:12:43 +0530

    added a reize event listener to handle logo on mobile screen size.

--- src/components/Navbar/Logo.jsx
+++ src/components/Navbar/Logo.jsx
@@ -1,15 +1,35 @@
 import { css } from "@emotion/react";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Logo = () => (
-  <h2 css={styles}>
-    <Link to="/" style={{ color: "inherit" }}>
-      TRAINING <span>STUDIO</span>
-    </Link>
-  </h2>
-);
+const Logo = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 400);
+
+  {
+    /* Performs similarly to componentDidMount in classes */
+  }
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => {
+        const ismobile = window.innerWidth < 400;
+        if (ismobile !== isMobile) setIsMobile(ismobile);
+      },
+      false
+    );
+    return () => {
+      window.removeEventListener("resize", () => {});
+    };
+  }, [isMobile]);
+  return (
+    <h2 css={styles}>
+      <Link to="/" style={{ color: "inherit" }}>
+        {isMobile ? "T" : "TRAINING"} <span>{isMobile ? "S" : "STUDIO"}</span>
+      </Link>
+    </h2>
+  );
+};
 
 const styles = css`
   color: #fff;
